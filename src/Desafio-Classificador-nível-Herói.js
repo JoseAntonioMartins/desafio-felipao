b26b277736afe81f5eaa8d196f4945f3b262955e
JoseAntonioMartins <diasmelhore97@gmail.com>
2023-10-31 21:34:59 -0300

    Desafio Classificador de nível de Heróis

---
+++ src/Desafio-Classificador-nível-Herói.js
@@ -0,0 +1,23 @@
+let nomeDoHeroi = "Homem_Aranha";
+let XP = 10002;
+let nivel;
+
+if (XP <= 1000) {
+    nivel = "Ferro";
+} else if (XP <= 2000) {
+    nivel = "Bronze";
+} else if (XP <= 5000) {
+    nivel = "Prata";
+} else if (XP <= 7000) {
+    nivel = "Ouro";
+} else if (XP <= 8000) {
+    nivel = "Platina";
+} else if (XP <= 9000) {
+    nivel = "Ascendente";
+} else if (XP <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log("O Herói de nome ${nomeDoHeroi} está no nível de ${nivel}");
